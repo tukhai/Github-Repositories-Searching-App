c0a029c0a91c9b99ad250dc657ebfe5157eaca76
Admin <admin@Admins-MacBook-Pro.local>
2019-03-30 00:03:54 +0800

    actually use fetch Github repo api, complete task 2 - prevent consume rate limit too quickly

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ import RepositoriesList from './components/RepositoriesList.js';
 import Pagination from './components/Pagination.js'
 import dummyData from './dummy-data.json';
 
-const PAGESIZE = 5;
+const PAGESIZE = 10;
 
 class App extends Component {
+  countdown;
+
   constructor(props) {
     super(props);
 
@@ -19,7 +21,8 @@ class App extends Component {
       repositoriesListData: [],
       listCount: 0,
       defaultSearchKeyword: defaultSearchKeywordFromQueryUrl,
-      currentPage: 1
+      currentPage: 1,
+      isShowLoadingIcon: true // initially it must be true, so when first load can see load icon
     };
     this.handleKeyUpSearch = this.handleKeyUpSearch.bind(this);
     this.handleChangePage = this.handleChangePage.bind(this);
@@ -28,36 +31,34 @@ class App extends Component {
   callSearchApi(searchKeyword, page) {
     if (searchKeyword !== "") {
       var fetchUrl = `https://api.github.com/search/repositories?q=${searchKeyword}&sort=stars&order=desc&page=${page ? page : "1"}&per_page=${PAGESIZE}`;
-      console.log(fetchUrl);
-      // fetch(fetchUrl,
-      // {
-      //   // credential: include
-      //   method: 'GET',
-      //   // headers: new Headers({
-      //   //   'SESS' : session_name + '=' + sessid,
-      //   //   'X-CSRF-TOKEN': token,
-      //   //   'Authorization': 'Basic '+btoa('edge:property'),
-      //   // }),
-      //   // timeout: TIMEOUT
-      // })
-      // .then((response) => response.json())
-      // .then((responseJson) => {
-      //   console.log(responseJson);
-      // })
-      // .catch((error) => {
-      //   console.error(error);
-      // });
-      console.log(dummyData);
-      if (dummyData && dummyData.items && dummyData.items.length > 0) {
+
+      fetch(fetchUrl,
+      {
+        method: 'GET'
+      })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        var dataList = [];
+        var dataCount = 0;
+        if (responseJson && responseJson.items && responseJson.items.length > 0) {
+          dataList = responseJson.items;
+          dataCount = responseJson.total_count;
+        }
+
         this.setState({
-          repositoriesListData: dummyData.items,
-          listCount: dummyData.total_count
+          repositoriesListData: dataList,
+          listCount: dataCount,
+          isShowLoadingIcon: false
         });
-      }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
     } else {
       this.setState({
         repositoriesListData: [],
-        listCount: 0
+        listCount: 0,
+        isShowLoadingIcon: false
       });
     }
   }
@@ -73,7 +74,24 @@ class App extends Component {
 
     // Call repository search API here
     // For task 2, have to do here, so we prevent callSearchApi as handleKeyUpSearch
-    this.callSearchApi(searchKeyword);
+
+    // Show the loading icon
+    if (!this.state.isShowLoadingIcon) {
+      this.setState({
+        isShowLoadingIcon: true
+      });
+    }
+
+    clearTimeout(this.countdown);
+    this.countdown = setTimeout(() => {
+      // When finish 3s, must call search API, show the tick icon inside call api funcion
+      this.callSearchApi(searchKeyword);
+
+      // When calling new api, should set currentPage to 1
+      this.setState({
+        currentPage: 1
+      });
+    }, 3000);
   }
 
   handleChangePage(page) {
@@ -81,7 +99,8 @@ class App extends Component {
     let currentSearchQuery = params.get('search') ? params.get('search') : "";
 
     this.setState({
-      currentPage: page
+      currentPage: page,
+      isShowLoadingIcon: true
     });
 
     this.callSearchApi(currentSearchQuery, page);
@@ -128,11 +147,28 @@ class App extends Component {
           <h2>Github Repository Search</h2>
 
           {/* TO DO: Can improve by using cookie to save previous search keywords and search list results */}
-          <SearchBox 
-            onKeyUpSearch = {this.handleKeyUpSearch}
-            defaultSearchKeyword = {this.state.defaultSearchKeyword}
-          />
+          <div>
+            <SearchBox
+              onKeyUpSearch = {this.handleKeyUpSearch}
+              defaultSearchKeyword = {this.state.defaultSearchKeyword}
+            />
+            <div className="icons-container">
+              {this.state.isShowLoadingIcon ?
+                <div className="lds-ring">
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                </div>
+                :
+                <div className="tick-icon">
+                  <span>&#10003;</span>
+                </div>
+              }
+            </div>
+          </div>
 
+          <div style={{clear: "both"}}></div>
           <h4>{statsText}</h4>
 
           <RepositoriesList 
@@ -140,12 +176,16 @@ class App extends Component {
           />
 
           <Pagination 
-            totalItems = {this.state.listCount}
+            totalItems = {this.state.listCount < 1000 ? this.state.listCount : 1000} // because this api can only show first 1000 results
             pageSize = {PAGESIZE}
             currentPage = {this.state.currentPage}
             maxPages = {10}
             onChangePage = {this.handleChangePage}
           />
+        </div>
+
+        <div className="instruction-content">
+          * As Github only allow first 1000 search results, although you can see more than 1000 results found, the app only show until the 1000th result.
         </div>
       </div>
     );
